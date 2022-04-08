ed808fb80ab9f3a04621277700fc29974854a9da
alikahwaji <ali.kahwaji@hotmail.com>
2022-04-08 17:45:08 +1200

    First class example

---
+++ src/class.js
@@ -0,0 +1,40 @@
+//TODO
+/*----------------------------------------------------------------
+Create functions for the Calculator class that can do the following:
+1- Add two numbers.
+2- Subtract two numbers.
+3- Multiply two numbers.
+4- Divide two numbers.
+
+const  calculator = new Calculator()
+
+calculator.add(10, 5) ➞ 15
+
+calculator.subtract(10, 5) ➞ 5
+
+calculator.multiply(10, 5) ➞ 50
+
+calculator.divide(10, 5) ➞ 2
+
+------------------------------------------------------------------*/
+
+//ANCHOR solution version 1.0.0
+class Calculator {
+	// Write functions to add(), subtract(), multiply() and divide()
+ add(num1,num2){
+		return num1+num2;
+	}
+	subtract(num1,num2){
+		return num1-num2;
+	}
+	multiply(num1,num2){
+		return num1*num2;
+	}
+	divide(num1,num2){
+		return num1/num2;
+	}
+}
+/*----------------------------------------------------------------*/
+
+//TODO:
+
